07f37384dcf6d9d4f5208fc02ac5aa051364bb0c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-24 12:50:16 +0300

    added search and products tests

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Search, type: :model do
+    it "is created successfully with a search term" do
+        search=Search.create(search_term:"Shoes")
+        expect(search).to be_valid
+    end
+
+    it{should has_many(:products)}
+
+    describe "Validations" do
+        it{is_expected.to validate_presence_of(:search_term)}
+    end
+end
