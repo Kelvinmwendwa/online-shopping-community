7c537463d5c32c9d39c7f56154762cc2ce9dbcbc
Brian Wangila <bwanjalah@gmail.com>
2022-10-24 16:13:47 +0300

    finish amazon scrape

--- app/crawlers/pigia_me.rb
+++ app/crawlers/pigia_me.rb
@@ -18,13 +18,16 @@ class PigiaMe
     end
 
     def products
-      articles = @parsed_page.xpath("//div[@class='listings-cards']/div")
+      articles = @parsed_page.xpath(".//div[@class='listing-card listing-card--tab listing-card--has-contact listing-card--has-content']/a")
 
       articles.map do |product|
         product = {
-          img: product.xpath(".//div[@class='listing-card_image_inner']/img")
-          name: product.xpath(".//div[@class='listing-card_header-content']/div")
+          img: product.xpath(".//img[@class='listing-card__image__resource vh-img']").attr("src").to_s,
+          name: product.xpath(".//div[@class='listing-card__header__title']/text()").to_s,
+          price: product.xpath(".//span[@class='listing-card__price']/span/text()").to_s,
+          
         }
+
       end
     end
     
