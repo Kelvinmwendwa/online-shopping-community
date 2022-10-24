1b71225dbdc94ba6e1e2387734cd9a707e3b8891
gatwi <faith.murithi@student.moringaschool.com>
2022-10-24 15:16:01 +0300

    updated controller and serializer for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+
+    def new
+        @user = User.new
+    end 
+
+    def index
+    end 
+
+    def show
+        @user = User.find_by(id: session[:user_id])
+        if @user 
+            render json: @user 
+        else 
+            render json: { error: "Not authorized" }, status: :unauthorized 
+        end
+    end 
+
+    def create 
+        @user = User.new(user_params) 
+        if @user.save and @user.valid?
+            session[:user_id] = @user.id 
+            render 'users/new' 
+        else 
+            render :action => "new" 
+        end
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:name, :username, :email, :location, :password_digest )
+    end 
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :username, :email, :location, :password_digest
+  
 end
