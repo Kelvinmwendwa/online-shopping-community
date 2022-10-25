865a9f1124a45f0dcd84e68ffb98e0eb565f7261
gatwi <faith.murithi@student.moringaschool.com>
2022-10-25 15:48:38 +0300

    user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
+    validates :name, :username, presence: true, uniqueness: true 
+
+
+    has_secure_password
+    validates :email,
+        format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+    
+
 end
