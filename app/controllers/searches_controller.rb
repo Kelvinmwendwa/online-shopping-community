641a4bd17a2681ab370e221872c592598ad52e42
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-25 09:44:20 +0300

    added create pror find by

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,10 @@
 class SearchesController < ApplicationController
 
     def create
-        search=Search.create!(search_term: params[:search])
+        search=Search.find_by(search_term:params[:search_term])
+
+        search=Search.create(search_term: params[:search_term]) unless search
+        
         render json: search.products, status: :ok
     end
 end
