18d380da14be63d9d9d298ab12f4b8480460771e
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-27 20:06:05 +0300

    add trend action

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,14 +3,23 @@ class SearchesController < ApplicationController
     before_action :authorize, only:[:index]
 
     def create
-        search=Search.find_by(search_term:params[:search_term])
+        term=params[:search_term].downcase
+        search=Search.find_by(search_term:term)
 
-        search=Search.create(search_term: params[:search_term]) unless search
+        
+        search.update(count: search.count+1) if search
+
+        search=Search.create(search_term: term) unless search
 
         UserSearch.create(user_id:current_user.id,search_id:search.id) if logged_in?
         
         render json: search.products, status: :ok
     end
+
+    def trends
+       render json: Search.all.order_by_count.limit(12)
+    end
+
 
     def index
         render json: current_user.searches.uniq
