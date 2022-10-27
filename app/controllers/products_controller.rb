edb46cd06ba28bb6403d49b0c06cfd6de2299b0e
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-27 20:05:49 +0300

    add trend actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,12 @@ class ProductsController < ApplicationController
 
         render json: product, status: :ok
     end
+    def trending
+        search=Search.find_by(id: params[:search_id])
+
+        render json: search.products
+    end
+    def toptrends
+        render json: Search.all.order_by_count.limit(1).first.products
+    end
 end
