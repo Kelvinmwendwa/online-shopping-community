413e160bdc6a794a3d36067fc46ac1165e8b5d8d
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-29 12:43:49 +0300

    bugs fix

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module OnlineShoppingCommunity
     # config.eager_load_paths << Rails.root.join("extras")
 
    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # config.assets.version = '1.0'
 
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
