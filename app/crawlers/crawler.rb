c27aa924eea2a065509b3484cf2709e1bb6e2ec2
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-10-31 14:23:54 +0300

    add crawler_helper -factor

--- app/crawlers/crawler.rb
+++ app/crawlers/crawler.rb
@@ -7,25 +7,27 @@ require 'nokogiri'
 require 'net/http'
 
 class Crawler
+  include CrawlerHelper
+
   def initialize(search, search_id)
     key = '9lSwt8nLosyS3gbo5mUswDFEXIjLGAgf'
 
-    url_jumia = URI("https://api.webscrapingapi.com/v1?url=https%3a%2f%2fwww.jumia.co.ke%2fcatalog%2f%3fq%3d#{search}&api_key=#{key}&render_js=1&wait_until=networkidle2")
-    url_amazon = URI("https://api.webscrapingapi.com/v1?url=https%3a%2f%2fwww.amazon.com%2fs%3fk%3d#{search}&api_key=#{key}&render_js=1&wait_until=domcontentloaded")
-    url_ebay = URI("https://api.webscrapingapi.com/v1?url=https%3a%2f%2fwww.ebay.com%2fsch%2fi.html%3f_from%3dR40%26_trksid%3dp2380057.m570.l1313%26_nkw%3d#{search}&api_key=#{key}")
+    @url_jumia = URI("https://api.webscrapingapi.com/v1?url=https%3a%2f%2fwww.jumia.co.ke%2fcatalog%2f%3fq%3d#{search}&api_key=#{key}&render_js=1&wait_until=networkidle2")
+    @url_amazon = URI("https://api.webscrapingapi.com/v1?url=https%3a%2f%2fwww.amazon.com%2fs%3fk%3d#{search}&api_key=#{key}&render_js=1&wait_until=domcontentloaded")
+    @url_ebay = URI("https://api.webscrapingapi.com/v1?url=https%3a%2f%2fwww.ebay.com%2fsch%2fi.html%3f_from%3dR40%26_trksid%3dp2380057.m570.l1313%26_nkw%3d#{search}&api_key=#{key}")
 
     @search_id = search_id
-    @pages = {
-      jumia: response(url_jumia),
-      ebay: response(url_ebay),
-      amazon: response(url_amazon)
-    }
+    # @pages = {
+    #   jumia: response(url_jumia),
+    #   ebay: response(url_ebay),
+    #   amazon: response(url_amazon)
+    # }
   end
 
   def response(url)
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Get.new(url)
 
@@ -38,7 +40,7 @@ class Crawler
   end
 
   def jumia
-    articles = @pages[:jumia].xpath("//div[@class='-paxs row _no-g _4cl-3cm-shs']/article/a")
+    articles = response(@url_jumia).xpath("//div[@class='-paxs row _no-g _4cl-3cm-shs']/article/a")
     raw = articles.map.with_index do |product, index|
       price = product.xpath(".//div[@class='prc']/text()").to_s
       product = {
@@ -48,17 +50,18 @@ class Crawler
         price_before_discount: product.xpath(".//div[@class='old']/text()").to_s,
         discount: product.xpath(".//div[@class='bdg _dsct _sm']/text()").to_s,
         ratings: count_stars(product.xpath(".//div[@class='stars _s']/text()").to_s),
+        rated_products:product.xpath(".//div[@class='rev']/text").to_s.scan(/\d+/).first.to_i,
         shop: 'jumia',
         price_index: calculate_price_index(float_price(price), index),
-        search_id: @search_id
+        search_id: @search_id,
+        product_url: @url_jumia
       }
     end.slice(0, 6)
     create_products(raw)
   end
 
   def ebay
-    cards = @pages[:ebay].xpath("//div[@class='s-item__wrapper clearfix']")
-
+    cards = response(@url_ebay).xpath("//div[@class='s-item__wrapper clearfix']")
     raw = cards.map.with_index do |card, index|
       price = card.xpath(".//span[@class='s-item__price']/text()").to_s
       {
@@ -74,15 +77,15 @@ class Crawler
         coupon_discount: card.xpath(".//span[@class='NEGATIVE BOLD']/text()").to_s,
         shop: 'ebay',
         price_index: calculate_price_index(dollar_price(price), index),
-        search_id: @search_id
+        search_id: @search_id,
+        product_url: @url_ebay
       }
     end.slice(1, 7)
     create_products(raw)
   end
 
   def amazon
-    articles = @pages[:amazon].xpath(".//div[@class='a-section a-spacing-base']")
-
+    articles = response(@url_amazon).xpath(".//div[@class='a-section a-spacing-base']")
     raw = articles.map.with_index do |product, index|
       price = product.xpath(".//span[@class='a-price']/span/text()").to_s
       {
@@ -92,9 +95,11 @@ class Crawler
         price_before_discount: product.xpath(".//span[@class='a-price a-text-price']/span[1]/text()").to_s,
         ratings: count_stars(product.xpath(".//i[@class='a-icon a-icon-star-small a-star-small-4-5 aok-align-bottom']/span/text()").to_s),
         rated_products: product.xpath(".//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style']/span/text()").to_s,
+        products_sold:"",
         shop: 'amazon',
         price_index: calculate_price_index(dollar_price(price), index),
-        search_id: @search_id
+        search_id: @search_id,
+        product_url: @url_amazon
       }
     end.slice(0, 6)
     create_products(raw)
@@ -104,27 +109,5 @@ class Crawler
     raw_products&.map { |p| Product.create(p) }
   end
 
-  private
-
-  def count_stars(stars)
-    s = stars.scan(/[1-5]/)
-    if s[1] == '.'
-      s = s.slice(0, 3)
-      s.join('').to_f.round
-    else
-      s.first.to_i
-    end
-  end
-
-  def float_price(price)
-    price.scan(/[0-9.]+/).first.to_f
-  end
-
-  def dollar_price(price)
-    float_price(price) * 100
-  end
-
-  def calculate_price_index(price, index)
-    (10 * index) + price / 10
-  end
+ 
 end
