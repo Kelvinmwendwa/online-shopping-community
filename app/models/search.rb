57c49b2413a0fc596673dfe43bba09f3df2d3211
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-11-02 20:19:27 +0300

    add association

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,6 +3,7 @@
 class Search < ApplicationRecord
   scope :order_by_count, -> { order(count: :desc) }
   has_many :products
+  has_many :user_searches
   validates :search_term, presence: true
 
   after_create :crawl
