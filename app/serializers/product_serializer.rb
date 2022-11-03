90e96e3b1bf1c51323e15aac43e8d9f3152539f5
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-11-03 10:02:03 +0300

    add normal price

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,5 +2,5 @@
 
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :price_before_discount, :discount, :shop, :image_url, :ratings, :rated_products,
-             :return_policy, :shipping, :coupon_discount, :rated_products, :product_url
+             :return_policy, :shipping, :coupon_discount, :rated_products, :product_url, :price_index,:price_normal
 end
