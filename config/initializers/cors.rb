1a769088f84cc74c04bf0e8f3046bc3db4cb0442
Kelvin <kmwendwa476@gmail.com>
2022-11-03 12:28:27 +0300

    merged a conflict

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,14 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-<<<<<<< HEAD
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-=======
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
->>>>>>> 90e96e3b1bf1c51323e15aac43e8d9f3152539f5
   end
 end
